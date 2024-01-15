9792a8e4cf69d82b71491cf334ad598be7136d6b
joao <zala4453523@gmail.com>
2024-01-15 16:03:59 -0300

    V1 (Primeiras impressões sobre o Nuxt)

---
+++ app.vue
@@ -0,0 +1,26 @@
+<script setup>
+useHead({
+  titleTemplate: (titleChunk) => {
+    return titleChunk
+      ? `${titleChunk} - João Gabriel Portfolio`
+      : "João Gabriel Portfolio";
+  },
+});
+</script>
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <AppAlert> This is an auto-imported component. </AppAlert>
+    <NuxtPage />
+    <img src="/img/favicon.ico" alt="Discover Nuxt 3" />
+    <img src="/assets/img/favicon.ico" alt="Discover Nuxt 3" />
+    <header>
+      <nav>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+  </div>
+</template>
---
+++ pages/about.vue
@@ -0,0 +1,10 @@
+<script setup>
+useHead({
+  title: "Sobre",
+});
+</script>
+<template>
+  <section>
+    <p>This page will be displayed at the /about route.</p>
+  </section>
+</template>
